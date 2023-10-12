333ced0a6dbe94c4c921bdad1c5b3577238a601b
Sagnik Ghosh <sagnik@worqhat.com>
2023-10-13 01:21:42 +0530

    fix: Update the process of run queries

--- src/databases/fetch-data/run-queries.ts
+++ src/databases/fetch-data/run-queries.ts
@@ -1,14 +1,26 @@
-export const fetchWithCondition = (
-  collectionName: string,
+import { appConfiguration } from '../../index';
+import axios from 'axios';
+import { handleAxiosError } from '../../error';
+import {
+  createLogger,
+  baseUrl,
+  debug,
+  LogStatus,
+  startProcessingLog,
+  stopProcessingLog,
+} from '../../core';
+
+export const fetchWithCondition = async (
+  name: string,
   whereQuery: { field: string; operator: string; value: any }[],
   joinStatement?: string,
   orderBy?: string,
   order?: 'asc' | 'desc' | null,
   limit?: number | null,
-): Promise<any[]> => {
+) => {
   console.log(
     'fetchWithCondition',
-    collectionName,
+    name,
     whereQuery,
     joinStatement,
     orderBy,
@@ -16,7 +28,74 @@ export const fetchWithCondition = (
     limit,
   );
 
-  return new Promise((resolve, reject) => {
-    resolve([]);
-  });
+  debug(
+    LogStatus.INFO,
+    `Retrieve Data from Collection ${name}`,
+    `Retrieving data from collection ${name}`,
+  );
+  if (!name) {
+    debug(
+      LogStatus.ERROR,
+      'Retrieve Data from Collection',
+      `Collection Name is missing`,
+    );
+    throw new Error('Collection Name is required');
+  }
+
+  if (!appConfiguration) {
+    debug(
+      LogStatus.ERROR,
+      'Retrieve Data from Collection',
+      `App Configuration is null`,
+    );
+    throw new Error('App Configuration is null');
+  }
+
+  try {
+    debug(
+      LogStatus.INFO,
+      `Retrieve Data from Collection ${name}`,
+      `Retrieving data from collection ${name}`,
+    );
+    startProcessingLog(
+      `Retrieve Data from Collection ${name}`,
+      `Retrieving data from collection ${name}`,
+    );
+    const response = await axios.post(
+      `${baseUrl}/api/collections/data/fetch/query`,
+      {
+        collection: name,
+        queries: whereQuery,
+        compounding: joinStatement || '',
+        orderType: order || null,
+        orderBy: orderBy || null,
+        limit: limit || null,
+      },
+      {
+        headers: {
+          Authorization: 'Bearer ' + appConfiguration.apiKey,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    stopProcessingLog();
+    debug(
+      LogStatus.INFO,
+      `Retrieve Data from Collection ${name}`,
+      `Retrieving data from collection ${name}`,
+    );
+    return {
+      code: 200,
+      ...response.data,
+    };
+  } catch (error: any) {
+    stopProcessingLog();
+    debug(
+      LogStatus.ERROR,
+      `Retrieve Data from Collection ${name}`,
+      `Error retrieving data from collection ${name}`,
+    );
+    throw handleAxiosError(error);
+  }
 };
